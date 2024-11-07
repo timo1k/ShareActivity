7e5a65fa8e4a5036538d8c44eec14d3e5cfdeb84
Timothy <timkhumpan@gmail.com>
2024-11-07 14:51:48 -0500

    create and starte implicit intent

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,9 +1,12 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,14 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+
+            if (editText.text.toString().isNotEmpty()){
+                val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                    putExtra(Intent.EXTRA_TEXT, editText.text)
+                }
+                startActivity(Intent.createChooser(shareIntent, "Share text via"))
+
+            }
 
         }
     }
